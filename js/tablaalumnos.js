9c2d35b604f3266de9eaac2139293cd3f886cf8a
jorgesr92 <jr_6924@hotmail.com>
2018-03-21 18:06:24 +0100

    commit 1

---
+++ js/tablaalumnos.js
@@ -0,0 +1,31 @@
+function verificardni () {
+    var DNI= document.getElementById("DNIalu").value;
+    var numDNI=parseInt((DNI.substing(0,7)));
+    var letra = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'];
+    //con el final el array no se puede modificar 
+    var i=(numDNI%23);
+    return DNI.substring(0,7)+letra[i];
+}
+function error() {
+    var DNI= document.getElementById("DNIalu").value;
+    var DNIverificado=verificardni();
+    if (DNI!=DNIverificado){
+        alert("El DNI introducido ("+DNI+") no es correcto, vuelve a introducirlo.");
+        return false;
+    } if (DNI==DNIverificado){
+        return true;
+    }
+}
+function agregarlumno() {
+    var DNI= document.getElementById("DNIalu").value;
+    var NomAlu= document.getElementById("NOMalu").value;
+    var Apealu= document.getElementById("APEalu").value;
+    var correoalu= document.getElementById("CORREOalu").value;
+    var Notaalu= document.getElementById("NOTAalu").value;
+    if (error()) {
+        document.getElementById("resultat").innerHTML = "todo good "+DNI+Nomalu+Apealu+correoalu+Notaalu;
+        //<table class='table table-striped'><tr> <td>"+DNI+"</td>  <td>"+NomAlu+"</td> <td>"+Apealu+"</td> <td>"+correoalu+"</td><td>"+Notaalu+"</td></tr></table>
+    }
+    return false
+}
+
